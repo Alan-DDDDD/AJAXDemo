10063ff9872f740fb173311f9209df2e613039fe
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-01-09 09:41:27 +0800

    HW2

---
+++ AJAXDemo/Controllers/HW2Controller.cs
@@ -0,0 +1,29 @@
+﻿using AJAXDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJAXDemo.Controllers
+{
+    public class HW2Controller : Controller
+    {
+        private readonly DemoContext _context;
+
+        public HW2Controller(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult login(string name)
+        {
+            var result = _context.Members.FirstOrDefault(c => c.Name == name);
+            if (result == null)
+                return Content($"0");
+            else return Content($"{result.Name}");
+
+        }
+    }
+}
